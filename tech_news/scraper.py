c78a4c431286a13787d80522d3c02b734bb806f4
mabiiak <mabiane.polniak@gmail.com>
2022-08-29 18:47:54 -0300

    feat: create function scrape_next_page_link

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -31,7 +31,9 @@ def scrape_novidades(html_content):
 
 # Requisito 3
 def scrape_next_page_link(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    link_button = selector.css(".nav-links .next::attr(href)").get()
+    return link_button
 
 
 # Requisito 4
