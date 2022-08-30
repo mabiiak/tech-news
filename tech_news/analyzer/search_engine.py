428042aeb67dc0ff6f922bf5000b62bbf97c1fa9
mabiiak <mabiane.polniak@gmail.com>
2022-08-30 18:58:56 -0300

    feat: implement search_by_date

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,4 +1,5 @@
 from tech_news.database import search_news
+from datetime import datetime
 
 
 # Requisito 6
@@ -16,7 +17,19 @@ def search_by_title(title):
 
 # Requisito 7
 def search_by_date(date):
-    """Seu código deve vir aqui"""
+    try:
+        find_by_date = list()
+        date = datetime.strptime(date, '%Y-%m-%d')
+        default_date = datetime.strftime(date, '%d/%m/%Y')
+        new_date = search_news({
+            'timestamp': default_date
+        })
+        for date in new_date:
+            find_by_date.append((date['title'], date['url']))
+
+    except ValueError:
+        raise ValueError('Data inválida')
+    return find_by_date
 
 
 # Requisito 8
