fa9d881bcf609468cedc4e5769fd77dab837d2b5
mabiiak <mabiane.polniak@gmail.com>
2022-08-31 08:42:27 -0300

    feat: implement search_by_category

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -39,4 +39,8 @@ def search_by_tag(tag):
 
 # Requisito 9
 def search_by_category(category):
-    """Seu código deve vir aqui"""
+    find = search_news(
+        {"category": {"$regex": category, "$options": "i"}}
+    )
+
+    return [(item["title"], item["url"]) for item in find]
